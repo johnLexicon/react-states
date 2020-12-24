b579dcdf9bf95f583f26bfd553291ece0f489879
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-12-24 13:52:55 +0100

    Added links and page not found.

--- src/pages.js
+++ src/pages.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function Home() {
   return (
     <div>
       <h1>Company Website</h1>
+      <ul style={{ textAlign: 'left' }}>
+        <li>
+          <Link to="about">About</Link>
+        </li>
+        <li>
+          <Link to="events">Events</Link>
+        </li>
+        <li>
+          <Link to="contact">Contact</Link>
+        </li>
+      </ul>
     </div>
   );
 }
@@ -28,6 +40,14 @@ export function Contact() {
   return (
     <div>
       <h1>[Contact]</h1>
+    </div>
+  );
+}
+
+export function Whoops404() {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
     </div>
   );
 }
