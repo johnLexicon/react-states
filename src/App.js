04ea2f46d4c4df1677a0628dc4ae9fd243d80cc7
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-12-24 12:53:21 +0100

    Added routes for components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { useReducer } from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Home, Contact, Events, About } from './pages';
 
 function App() {
   return (
     <div className="App">
-      <h3>Hello React Testing</h3>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
