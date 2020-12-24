2ec23fac5a9a223c2e9ef5f75a6306ddef0c4eb8
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-12-24 08:29:43 +0100

    Example, telling useState on which state property it should run.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import './App.css';
 
 function App() {
   const [mood, setMood] = useState('happy');
+  const [secondMood, setSecondMood] = useState('tired');
 
   useEffect(() => {
     console.log(mood);
-  }, []); //Empty array makes the useEffect be called only once.
+  }, [mood]);
 
   return (
     <div className="App">
-      <h2>{mood}</h2>
+      <h2>
+        {mood} and {secondMood}
+      </h2>
       <button onClick={() => setMood('happy')}>Happy</button>
       <button onClick={() => setMood('frustrated')}>Frustrated</button>
       <button onClick={() => setMood('sad')}>Sad</button>
+      <button onClick={() => setSecondMood(['tired', 'exhausted'][Math.floor(Math.random() * 2)])}>
+        Random second mood
+      </button>
     </div>
   );
 }
